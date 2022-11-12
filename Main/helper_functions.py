43d09d3774c1140717d734120dc30fc522b47664
Sila Shivanandan <silashivanandan2000@gmail.com>
2022-11-12 18:00:22 +0530

    second commit

--- Main/helper_functions.py
+++ Main/helper_functions.py
@@ -14,24 +14,6 @@ from tensorflow.keras import layers
 IMAGE_SHAPE = (224, 224)
 BATCH_SIZE = 32
 
-# View an image
-def view_random_image(target_dir, target_class):
-  # Setup target directory (we'll view images from here)
-  target_folder = target_dir+target_class
-
-  # Get a random image path
-  random_image = random.sample(os.listdir(target_folder), 1)
-
-  # Read in the image and plot it using matplotlib
-  img = mpimg.imread(target_folder + "/" + random_image[0])
-  plt.imshow(img)
-  plt.title(target_class)
-  plt.axis("off");
-
-  print(f"Image shape: {img.shape}") # show the shape of the image
-
-  return img
-
 # Plot the validation and training data separately
 def plot_loss_curves(history):
   """
@@ -39,19 +21,15 @@ def plot_loss_curves(history):
   """ 
   loss = history.history['loss']
   val_loss = history.history['val_loss']
-
   accuracy = history.history['accuracy']
   val_accuracy = history.history['val_accuracy']
-
   epochs = range(len(history.history['accuracy']))
-
   # Plot loss
   plt.plot(epochs, loss, label='training_loss')
   plt.plot(epochs, val_loss, label='val_loss')
   plt.title('Loss')
   plt.xlabel('Epochs')
   plt.legend()
-
   # Plot accuracy
   plt.figure()
   plt.plot(epochs, accuracy, label='training_accuracy')
@@ -67,22 +45,18 @@ def create_model(model_url, num_classes=5):
                                            trainable=False, # freeze the underlying patterns
                                            name='feature_extraction_layer',
                                            input_shape=IMAGE_SHAPE+(3,)) # define the input image shape
-  
   # Create our own model
   model = tf.keras.Sequential([
     feature_extractor_layer, # use the feature extraction layer as the base
     layers.Dense(num_classes, activation='softmax') # create our own output layer      
   ])
-
   return model
 
 # Create a function to import an image and resize it to be able to be used with our model
 def load_image(filename):
   # Read in target file (an image)
   img = tf.io.read_file(filename)
-
   img = tf.image.decode_image(img, channels=3)
-
   # Resize the image (to the same size our model was trained on)
   img = (tf.image.resize(img, size = IMAGE_SHAPE))/255.
   # Rescale the image (get all values between 0 and 1)
@@ -90,19 +64,15 @@ def load_image(filename):
 
 # Adjust function to work with multi-class
 def prediction(model, filename, class_names):
-
   # Import the target image and preprocess it
   img = load_image(filename)
-
   # Make a prediction
   pred = model.predict(tf.expand_dims(img, axis=0))
-
   # Get the predicted class
   if len(pred[0]) > 1: # check for multi-class
     pred_class = class_names[pred.argmax()] # if more than one output, take the max
   else:
     pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
-
   # Plot the image and predicted class
   plt.imshow(img)
   plt.title(f"Prediction: {pred_class}")
