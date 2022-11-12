fc68597dbce6d7d231957db987c8be5066f8c98f
Sila Shivanandan <silashivanandan2000@gmail.com>
2022-11-12 10:18:55 +0530

    initial commit

--- Main/helper_functions.py
+++ Main/helper_functions.py
@@ -77,29 +77,24 @@ def create_model(model_url, num_classes=5):
   return model
 
 # Create a function to import an image and resize it to be able to be used with our model
-def load_and_prep_image(filename, img_shape=224):
+def load_image(filename):
   # Read in target file (an image)
   img = tf.io.read_file(filename)
 
-  # Decode the read file into a tensor & ensure 3 colour channels 
-  # (our model is trained on images with 3 colour channels and sometimes images have 4 colour channels)
   img = tf.image.decode_image(img, channels=3)
 
   # Resize the image (to the same size our model was trained on)
-  img = tf.image.resize(img, size = [img_shape, img_shape])
+  img = tf.image.resize(img, size = IMAGE_SHAPE)
 
   # Rescale the image (get all values between 0 and 1)
   img = img/255.
   return img
 
 # Adjust function to work with multi-class
-def pred_and_plot(model, filename, class_names):
-  """
-  Imports an image located at filename, makes a prediction on it with
-  a trained model and plots the image with the predicted class as the title.
-  """
+def prediction(model, filename, class_names):
+
   # Import the target image and preprocess it
-  img = load_and_prep_image(filename)
+  img = load_image(filename)
 
   # Make a prediction
   pred = model.predict(tf.expand_dims(img, axis=0))
